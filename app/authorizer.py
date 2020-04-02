d859416a2333079efec3df4e6a7395da74f79e45
Myles Loffler <myles@britecore.com>
2020-04-02 18:39:38 -0500

    Custom authorizer now works
    This commit implements a working custom authorizer which only yields
    access when the appropriate secrets are provided as expected in
    the request.
    
    To enable this functionality, it also introduces several new roles
    which permit the Lambda functions involved to access the secret in
    AWS SecretsManager which contains the auth token generated for
    Okta to provide to this application.

--- app/authorizer.py
+++ app/authorizer.py
@@ -1,16 +1,44 @@
 import json
 import boto3
 
-print("Getting secret...")
-AUTH_SECRET = retrieve_secret()
-print(AUTH_SECRET)
+EFFECT_DENY = 'Deny'
+EFFECT_ALLOW = 'Allow'
 
-def retrieve_secret():
+def retrieve_auth_token():
     client = boto3.client('secretsmanager')
-    return client.get_secret_value(
+    secret =  client.get_secret_value(
         SecretId="app/okta-to-aws-sso"
     )
+    return json.loads(secret['SecretString'])['auth_token_for_okta']
+
+AUTH_TOKEN = retrieve_auth_token()
 
 def lambda_handler(event, context):
     print(json.dumps(event))
-    return None
+    provided_token = event['headers']['Authorization']
+    principal_id = event['headers']['x-api-key']
+    resource = event['methodArn']
+
+    policy = generate_policy(principal_id, EFFECT_DENY, resource)
+    if provided_token == AUTH_TOKEN:
+        policy = generate_policy(principal_id, EFFECT_ALLOW, resource)
+
+    print(json.dumps(policy))
+
+    return policy
+
+def generate_policy(principal_id, effect, resource):
+    return {
+        "principalId": principal_id,
+        "policyDocument": {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Action": "execute-api:Invoke",
+                    "Effect": effect,
+                    "Resource": resource
+                }
+            ]
+        }
+
+    }
